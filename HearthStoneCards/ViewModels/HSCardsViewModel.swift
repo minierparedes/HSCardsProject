60b94e7776e5dd4bd36ba91d7925f4da66fd4fd9
minierparedes <3thancr0wn@gmail.com>
2021-05-30 00:12:04 +0900

    Updated: HSCardsViewModel.swift: Further filtered fetch request. Brought in 3 new assests.

--- HearthStoneCards/ViewModels/HSCardsViewModel.swift
+++ HearthStoneCards/ViewModels/HSCardsViewModel.swift
@@ -54,114 +54,85 @@ class HSCardsViewModel: Identifiable, ObservableObject {
             .sink { [weak self] (receivedHSCardModel) in
                 self?.standardCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ self!.standardCardSetNamesYear2021.contains($0.set ?? "") && $0.name != "???" })
+                    .filter({ self!.standardCardSetNamesYear2021.contains($0.set ?? "") && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.wildCardSetNamesYear2021DATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ self!.wildCardSetNamesYear2021.contains($0.set ?? "") && $0.name != "???" })
+                    .filter({ self!.wildCardSetNamesYear2021.contains($0.set ?? "") && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.theBarrensCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "THE_BARRENS" && $0.name != "???" })
+                    .filter({ $0.set == "THE_BARRENS" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.darkmoonFaireCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "DARKMOON_FAIRE" && $0.name != "???" })
+                    .filter({ $0.set == "DARKMOON_FAIRE" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.scholomanceCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "SCHOLOMANCE" && $0.name != "???" })//from here down
+                    .filter({ $0.set == "SCHOLOMANCE" && $0.name != "???" && $0.type != "HERO_POWER" })// HERO_POWER
                 self?.blackTempleCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "BLACK_TEMPLE" && $0.name != "???" })
+                    .filter({ $0.set == "BLACK_TEMPLE" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.coreCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "CORE" && $0.name != "???" })
+                    .filter({ $0.set == "CORE" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.demonHunterInitiateCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "DEMON_HUNTER_INITIATE" && $0.name != "???" })
+                    .filter({ $0.set == "DEMON_HUNTER_INITIATE" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.naxxCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "NAXX" && $0.name != "???" })
+                    .filter({ $0.set == "NAXX" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.gvgCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "GVG" && $0.name != "???" })
+                    .filter({ $0.set == "GVG" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.brmCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "BRM" && $0.name != "???" })
+                    .filter({ $0.set == "BRM" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.tgtCardSetDATA = receivedHSCardModel//
                     .compactMap({ $0 })
-                    .filter({ $0.set == "TGT" && $0.name != "???" })
+                    .filter({ $0.set == "TGT" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.loeCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "LOE" && $0.name != "???" })
+                    .filter({ $0.set == "LOE" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.ogCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "OG" && $0.name != "???" })
+                    .filter({ $0.set == "OG" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.karaCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "KARA" && $0.name != "???" })
+                    .filter({ $0.set == "KARA" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.gangsCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "GANGS" && $0.name != "???" })
+                    .filter({ $0.set == "GANGS" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.ungoroCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "UNGORO" && $0.name != "???" })
+                    .filter({ $0.set == "UNGORO" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.iceCrownCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "ICECROWN" && $0.name != "???" })
+                    .filter({ $0.set == "ICECROWN" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.lootApaloozaCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "LOOTAPALOOZA" && $0.name != "???" })
+                    .filter({ $0.set == "LOOTAPALOOZA" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.gilneasCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "GILNEAS" && $0.name != "???" })
+                    .filter({ $0.set == "GILNEAS" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.boomsdayCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "BOOMSDAY" && $0.name != "???" })
+                    .filter({ $0.set == "BOOMSDAY" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.trollCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "TROLL" && $0.name != "???" })
+                    .filter({ $0.set == "TROLL" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.dalaranCardSetDATA = receivedHSCardModel//
                     .compactMap({ $0 })
-                    .filter({ $0.set == "DALARAN" && $0.name != "???" })
+                    .filter({ $0.set == "DALARAN" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.uldumCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "ULDUM" && $0.name != "???" })
+                    .filter({ $0.set == "ULDUM" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.dragonsCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "DRAGONS" && $0.name != "???" })
+                    .filter({ $0.set == "DRAGONS" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.yearOfTheDragonCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "YEAR_OF_THE_DRAGON" && $0.name != "???" })
+                    .filter({ $0.set == "YEAR_OF_THE_DRAGON" && $0.name != "???" && $0.type != "HERO_POWER" })
                 self?.legacyCardSetDATA = receivedHSCardModel
                     .compactMap({ $0 })
-                    .filter({ $0.set == "LEGACY" && $0.name != "???" })
-                
-            
-                print("*****\n theBarrensCardSetDATA \n***** element count: \(self?.theBarrensCardSetDATA.count)")
-                print("*****\n darkmoonFaireCardSetDATA \n***** element count: \(self?.darkmoonFaireCardSetDATA.count)")
-                print("*****\n standardCardSetDATA \n***** element count: \(self?.standardCardSetDATA.count)")
-                print("*****\n wildCardSetNamesYear2021DATA \n***** element count: \(self?.wildCardSetNamesYear2021DATA.count)")
-                print("*****\n theBarrensCardSetDATA \n***** element count: \(self?.scholomanceCardSetDATA.count)")
-                print("*****\n darkmoonFaireCardSetDATA \n***** element count: \(self?.blackTempleCardSetDATA.count)")
-                print("*****\n standardCardSetDATA \n***** element count: \(self?.coreCardSetDATA.count)")
-                print("*****\n wildCardSetNamesYear2021DATA \n***** element count: \(self?.demonHunterInitiateCardSetDATA.count)")
-                print("*****\n theBarrensCardSetDATA \n***** element count: \(self?.naxxCardSetDATA.count)")
-                print("*****\n darkmoonFaireCardSetDATA \n***** element count: \(self?.gvgCardSetDATA.count)")
-                print("*****\n standardCardSetDATA \n***** element count: \(self?.brmCardSetDATA.count)")
-                print("*****\n wildCardSetNamesYear2021DATA \n***** element count: \(self?.tgtCardSetDATA.count)")
-                print("*****\n theBarrensCardSetDATA \n***** element count: \(self?.loeCardSetDATA.count)")
-                print("*****\n darkmoonFaireCardSetDATA \n***** element count: \(self?.ogCardSetDATA.count)")
-                print("*****\n standardCardSetDATA \n***** element count: \(self?.karaCardSetDATA.count)")
-                print("*****\n wildCardSetNamesYear2021DATA \n***** element count: \(self?.gangsCardSetDATA.count)")
-                print("*****\n theBarrensCardSetDATA \n***** element count: \(self?.ungoroCardSetDATA.count)")
-                print("*****\n darkmoonFaireCardSetDATA \n***** element count: \(self?.iceCrownCardSetDATA.count)")
-                print("*****\n standardCardSetDATA \n***** element count: \(self?.lootApaloozaCardSetDATA.count)")
-                print("*****\n wildCardSetNamesYear2021DATA \n***** element count: \(self?.gilneasCardSetDATA.count)")
-                print("*****\n wildCardSetNamesYear2021DATA \n***** element count: \(self?.boomsdayCardSetDATA.count)")
-                print("*****\n theBarrensCardSetDATA \n***** element count: \(self?.trollCardSetDATA.count)")
-                print("*****\n darkmoonFaireCardSetDATA \n***** element count: \(self?.dalaranCardSetDATA.count)")
-                print("*****\n standardCardSetDATA \n***** element count: \(self?.uldumCardSetDATA.count)")
-                print("*****\n wildCardSetNamesYear2021DATA \n***** element count: \(self?.dragonsCardSetDATA.count)")
-                print("*****\n theBarrensCardSetDATA \n***** element count: \(self?.yearOfTheDragonCardSetDATA.count)")
-                print("*****\n darkmoonFaireCardSetDATA \n***** element count: \(self?.legacyCardSetDATA.count)")
+                    .filter({ $0.set == "LEGACY" && $0.name != "???" && $0.type != "HERO_POWER" })
                 
             }
             .store(in: &cancellables)
@@ -169,12 +140,6 @@ class HSCardsViewModel: Identifiable, ObservableObject {
         
      
 }
-
-
-/*
- .filter({ $0.set == "THE_BARRENS" && $0.cardClass == "NEUTRAL" && $0.cardClass == "DEMONHUNTER" && $0.cardClass == "DRUID" && $0.cardClass == "HUNTER" && $0.cardClass == "MAGE" && $0.cardClass == "PRIEST" && $0.cardClass == "WARLOCK" && $0.cardClass == "ROGUE" && $0.cardClass == "SHAMAN" && $0.cardClass == "WARRIOR" && $0.cardClass == "PALADIN" && $0.cardClass == "ROGUE"})
- */
-
 
 
 /*
--- HearthStoneCards/Views/CardsView/CoreCardsView.swift
+++ HearthStoneCards/Views/CardsView/CoreCardsView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct CoreCardsView: View {
+    @StateObject var hsCardsVM: HSCardsViewModel = HSCardsViewModel()
+    var dictionaryGroup: [String: [HSCardModel]] {
+        Dictionary(grouping: hsCardsVM.coreCardSetDATA, by: { $0.cardClass ?? "N/A" })
+    }
+    var uniq: [String] {
+        dictionaryGroup.map({ $0.key }).sorted()
+    }
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil)
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Rectangle()
+                .overlay(Image("Year_of_the_Gryphon_banner")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .clipped()
+                            .frame(width: 450, height: 200))
+                .frame(height: 200)
+            LazyVGrid(
+                columns: columns,
+                alignment: .center,
+                spacing: nil,
+                pinnedViews: [.sectionHeaders],
+                content: {
+                    ForEach(uniq, id: \.self) { section in
+                        Section(header:
+                                    SectionHeaderView(sectionTitle: section, sectionBadge: section)
+                        ) {
+                            ForEach(dictionaryGroup[section]!) {card in
+                                CardCellView(cardModel: card)
+                            }
+                        }
+                    }
+            })
+        }
     }
 }
 
